eb1ca51cb22d54f0ae9959d9c0779c957ce28de4
Raon <41016331+dydfuf@users.noreply.github.com>
2024-08-27 00:14:42 +0900

    Shadcn UI를 가져옵니다. (#1)
    * feat: tailwind 및 shadcn 기본 세팅을 합니다.
    
    * feat: shadcn 컴포넌트들을 추가합니다.
    
    * fix: eslint 에러를 수정합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import { Button } from "./components/ui/button";
+import { Toaster as SonnerToaster } from "./components/ui/sonner";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>hello world</Button>
+      <SonnerToaster />
+      <Toaster />
     </>
-  )
+  );
 }
 
 export default App
