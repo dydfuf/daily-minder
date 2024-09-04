08ba2f19caaca35d7cf194c1888d10df4a9f2b8a
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-04 23:06:24 +0900

    chore: 불필요한 파일을 제거합니다.

--- src/pages/editor/EditorPage.tsx
+++
@@ -1,8 +0,0 @@
-import { CustomEditor } from "@/components/common/CustomEditor";
-import { Route } from "@/routes/editor";
-
-export default function EditorPage() {
-  const content = Route.useLoaderData();
-
-  return <CustomEditor initialContent={content} />;
-}
