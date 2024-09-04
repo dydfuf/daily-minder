fdca5e9599f63dd85d1f51172a282231fa788036
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-04 23:07:38 +0900

    feat: auth route를 추가합니다. (#10)
    * feat: auth route를 추가합니다.
    
    * chore: 불필요한 파일을 제거합니다.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,15 @@
-import CommonLayout from "@/components/common/CommonLayout";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Toaster as SonnerToaster } from "../components/ui/sonner";
+import { Toaster } from "../components/ui/toaster";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <CommonLayout>
-        <Outlet />
-      </CommonLayout>
+      <Outlet />
       <TanStackRouterDevtools />
+      <SonnerToaster />
+      <Toaster />
     </>
   ),
 });
