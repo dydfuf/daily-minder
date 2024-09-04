cff60564365d6eddcfcc7977a3b361686306f549
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-05 00:35:34 +0900

    feat: tanstack query를 추가합니다.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,14 +2,21 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster as SonnerToaster } from "../components/ui/sonner";
 import { Toaster } from "../components/ui/toaster";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Outlet />
-      <TanStackRouterDevtools />
-      <SonnerToaster />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <TanStackRouterDevtools />
+        <SonnerToaster />
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   ),
 });
