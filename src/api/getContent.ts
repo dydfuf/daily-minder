e0fbf9bc55e27bd3fcecfd8a76cc68ff56edcb61
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-08 01:37:59 +0900

    feat: 데이터 불러오기 및 저장을 구현합니다.

--- src/api/getContent.ts
+++ src/api/getContent.ts
@@ -1,5 +1,9 @@
 import { supabase } from "@/lib/supabase";
 
-export const getContent = async (day: string) => {
+interface GetContentParams {
+  day: string;
+}
+
+export const getContent = async ({ day }: GetContentParams) => {
   return await supabase.from("content").select("*").eq("day", day);
 };
--- src/pages/daily/DailyPage.tsx
+++ src/pages/daily/DailyPage.tsx
@@ -1,15 +1,77 @@
-import { CustomEditor } from "@/components/common/CustomEditor";
-import { Route } from "@/routes/_auth/_auth.daily";
+import { createContent } from "@/api/createContent";
+import { getContent } from "@/api/getContent";
+import { updateContent } from "@/api/updateContent";
+import Editor from "@/components/editor/advanced-editor";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useSearch } from "@tanstack/react-router";
+import { JSONContent } from "novel";
+import { toast } from "sonner";
 
 export default function DailyPage() {
-  const { today } = Route.useSearch();
+  const { today } = useSearch({
+    from: "/_auth/_auth/daily",
+  });
+
+  const { data: initialContent, isPending } = useQuery({
+    queryKey: ["daily", today],
+    queryFn: async () => {
+      const { data, error } = await getContent({
+        day: today,
+      });
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      // 데이터가 없는경우 생성합니다.
+      if (!data?.[0]?.content) {
+        const { error } = await createContent({
+          day: today,
+        });
+        if (error) {
+          throw new Error(error.message);
+        }
+        return {};
+      }
+
+      const content = data[0].content ?? {};
+      return content;
+    },
+    staleTime: 0,
+    gcTime: 0,
+  });
+
+  const { mutateAsync: updateContentAsync } = useMutation({
+    mutationFn: async (content: JSONContent) => {
+      const { data, error } = await updateContent({
+        day: today,
+        content,
+      });
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+    onSuccess() {
+      toast.success("Content updated");
+    },
+  });
+
+  const handleUpdateContent = async (content: JSONContent) => {
+    await updateContentAsync(content);
+  };
 
   return (
-    <div>
+    <div className="p-12">
       <h1>{today}</h1>
-      <CustomEditor initialContent={defaultValue} />
+      {isPending && <EditorSkeleton />}
+      {!isPending && (
+        <Editor initialValue={initialContent} onChange={handleUpdateContent} />
+      )}
     </div>
   );
 }
 
-const defaultValue = {};
+const EditorSkeleton = () => {
+  return <Skeleton className="w-full h-[600px]" />;
+};
