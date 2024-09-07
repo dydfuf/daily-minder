9d9ff26717837094541ce1d24e0cfa81d4e804f3
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-07 22:38:46 +0900

    feat: search param을 받을 수 있도록 합니다.

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,6 +1,13 @@
 import LoginPage from "@/pages/login/LoginPage";
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import { fallback, zodSearchValidator } from "@tanstack/router-zod-adapter";
+
+const LoginPageSearchParamSchema = z.object({
+  redirect: fallback(z.string(), "/").default("/"),
+});
 
 export const Route = createFileRoute("/login")({
+  validateSearch: zodSearchValidator(LoginPageSearchParamSchema),
   component: () => <LoginPage />,
 });
