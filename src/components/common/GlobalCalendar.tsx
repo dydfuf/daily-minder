54dc07cab85c13935c37767514f514c9e6e48fc3
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-08 00:33:15 +0900

    feat: daily 페이지에서 today search param을 받을 수 있도록 합니다. (#16)

--- src/components/common/GlobalCalendar.tsx
+++ src/components/common/GlobalCalendar.tsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Calendar } from "../ui/calendar";
+import { useNavigate } from "@tanstack/react-router";
+import { format } from "date-fns";
 
 export default function GlobalCalendar() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+
+  const navigate = useNavigate({
+    from: "/daily",
+  });
+
+  useEffect(() => {
+    if (!date) {
+      return;
+    }
+
+    navigate({
+      search: {
+        today: format(date, "yyyy-MM-dd"),
+      },
+    });
+  }, [date, navigate]);
 
   return (
     <div className="p-8 border shadow">
