39d068bf6257a3b2a3a69e6ed0405a14227dab35
Raon <41016331+dydfuf@users.noreply.github.com>
2024-09-08 00:32:22 +0900

    feat: daily 페이지에서 today search param을 받을 수 있도록 합니다.

--- src/routes/_auth/_auth.daily.tsx
+++ src/routes/_auth/_auth.daily.tsx
@@ -1,5 +1,25 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import { fallback, zodSearchValidator } from "@tanstack/router-zod-adapter";
+import { format } from "date-fns";
+import DailyPage from "@/pages/daily/DailyPage";
+
+const today = format(new Date(), "yyyy-MM-dd");
+
+const DailyPageSearchParamSchema = z.object({
+  today: fallback(
+    z.string().transform((value) => {
+      try {
+        return format(value, "yyyy-MM-dd");
+      } catch {
+        return today;
+      }
+    }),
+    today
+  ).default(today),
+});
 
 export const Route = createFileRoute("/_auth/_auth/daily")({
-  component: () => <div>Hello /_auth/daily!</div>,
+  validateSearch: zodSearchValidator(DailyPageSearchParamSchema),
+  component: () => <DailyPage />,
 });
